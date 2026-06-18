2c9a60e8df0bdeec7535b29ce4ab681996a98cd7
buttercup-07 <golechhadiksha@gmail.com>
2026-06-18 21:00:39 +0530

    Feature:Login ; Backend: add authentication API and user management

---
+++ server/src/middleware/auth.js
@@ -0,0 +1,28 @@
+//all the code+comments written are cross verified with the documentation ; if u have any suggestions for improvement please let me know :)
+
+
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export const protect = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {//missing proper syntax of header
+    return res.status(401).json({ message: "Not authorized, token missing" });
+  }
+
+  try {
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);//check for valid token
+    const user = await User.findById(decoded.id).select("-password");//return all field except password
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Not authorized, token failed" });
+  }
+};
